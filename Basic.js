17767cc918e6581e530f27f69f65145b5b7c7ce2
Layton B <laytonb@hotmail.co.uk>
2019-12-17 10:08:37 +0000

    Add files via upload

---
+++ Basic.js
@@ -0,0 +1,184 @@
+var APIName = APIName || (function () {
+    var stateName = 'APIName',
+        states = [
+            ['Name', ['acceptable', 'Values'], 'defaultValue']
+        ],
+        name = 'API Name',
+        nameError = name + ' ERROR',
+        nameLog = name + ': ',
+
+        playerName,
+        playerID,
+
+        checkMacros = function () {
+            let playerList = findObjs({ _type: 'player', _online: true }),
+                gm = _.find(playerList, player => { return playerIsGM(player.id) === true; }),
+                macrosArr = [
+                    [
+                        'MacroName',
+                        'Macro Command'
+                    ]
+                ];
+            _.each(macrosArr, macro => {
+                let macroObj = findObjs({ _type: 'macro', name: macro[0] })[0];
+                if (macroObj) {
+                    if (macroObj.get('visibleto').includes('all') === false) {
+                        macroObj.set('visibleto', 'all');
+                        toChat(`**Macro '${macro[0]}' was made visible to all.**`, true);
+                    }
+                    if (macroObj.get('action') !== macro[1]) {
+                        macroObj.set('action', macro[1]);
+                        toChat(`**Macro '${macro[0]}' was corrected.**`, true);
+                    }
+                } else if (gm && playerIsGM(gm.id)) {
+                    createObj('macro', {
+                        _playerid: gm.id,
+                        name: macro[0],
+                        action: macro[1],
+                        visibleto: 'all'
+                    })
+                    toChat(`**Macro '${macro[0]}' was created and assigned to ${gm.get('_displayname')}.**`, true);
+                }
+            })
+        },
+
+        showHelp = function () {
+            let commandsArr = [
+                [
+                    '!command',
+                    'What it does.',
+                    `${code('!parameter1')} ${code('parameter2')}`,
+                    ['parameter1', 'How to use it.']
+                ]
+            ];
+            _.each(commandsArr, command => {
+                let output = `&{template:default} {{name=${code(command[0])} Help}}`;
+                _.each(command, function (part, index) {
+                    if (index < 3) {
+                        let section;
+                        switch (index) {
+                            case 0:
+                                section = 'Command';
+                                break;
+                            case 1:
+                                section = 'Function';
+                                break;
+                            case 2:
+                                section = 'Typical Input';
+                                break;
+                        }
+                        output += `{{${section}=${part}}}`;
+                    } else {
+                        output += `{{${part[0]}=${part[1]}}}`;
+                    }
+                })
+                toPlayer(output);
+            })
+            return;
+        },
+
+        showConfig = function () {
+            let output = `&{template:default} {{name=Area Saving Throw Config}}`;
+            _.each(states, value => {
+                let acceptableValues = value[1] ? value[1] : [true, false],
+                    defaultValue = value[2] ? value[2] : true,
+                    currentValue = `${getState(value[0])}`,
+                    stringVals = valuesToString(acceptableValues, defaultValue);
+                output += `{{${value[0]}=[${currentValue}](!ast config ${value[0]} ?{New ${value[0]} value${stringVals}})}}`;
+            })
+            toPlayer(output);
+            return;
+
+            function valuesToString(values, defaultValue) {
+                let output = '',
+                    index = values.indexOf(defaultValue);
+                if (index !== -1) {
+                    let val = values.splice(index, 1);
+                    values.unshift(val);
+                }
+                _.each(values, value => {
+                    output += `|${value}`;
+                })
+                return output;
+            }
+        },
+
+        handleInput = function (msg) {
+            playerName = msg.who.split(' ', 1)[0];
+            playerID = msg.playerid;
+            if (msg.type === 'api' && msg.content.split(' ')[0] === '!api') {
+                var parts = msg.content.split(' ');
+            }
+        },
+
+        getState = function (value) {
+            return state[`${stateName}_${value}`];
+        },
+
+        roll = function (formula) {
+            return new Promise(resolve => {
+                sendChat('', '/r ' + formula, results => {
+                    resolve(JSON.parse(results[0].content).total);
+                });
+            });
+        },
+
+        code = function (snippet) {
+            return `<span style="background-color: rgba(0, 0, 0, 0.5); color: White; padding: 2px; border-radius: 3px;">${snippet}</span>`;
+        },
+
+        toChat = function (message, success, target) {
+            let style = '<div>',
+                whisper = target ? `/w ${target} ` : '';
+            if (success === true) {
+                style = `<br><div style="background-color: #5cd65c; color: Black; padding: 5px; border-radius: 10px;">`;
+            } else if (success === false) {
+                style = `<br><div style="background-color: #ff6666; color: Black; padding: 5px; border-radius: 10px;">`;
+            }
+            sendChat(name, `${whisper}${style}${message}</div>`);
+        },
+
+        toPlayer = function (message, success) {
+            if (!success) {
+                sendChat(name, `/w ${playerName} ` + message);
+            } else {
+                sendChat(name, `/w ${playerName} ` + '<br><div style="background-color: #5cd65c; color: Black; padding: 5px; border-radius: 10px;">' + message + '</div>');
+            }
+        },
+
+        error = function (error, code) {
+            if (playerName) {
+                sendChat(nameError, `/w ${playerName} <br><div style="background-color: #ff6666; color: Black; padding: 5px; border-radius: 10px;">**${error}** Error code ${code}.</div>`);
+            } else {
+                sendChat(nameError, `<br><div style="background-color: #ff6666; color: Black; padding: 5px; border-radius: 10px;">**${error}** Error code ${code}.</div>`);
+            }
+            log(nameLog + error + ` Error code ${code}.`);
+        },
+
+        startupChecks = function () {
+            _.each(states, variable => {
+                let values = variable[1] ? variable[1] : [true, false],
+                    defaultValue = variable[2] ? variable[2] : true;
+                if (!state[`${stateName}_${variable[0]}`] || !values.includes(state[`${stateName}_${variable[0]}`])) {
+                    error(`**'${variable[0]}'** value **was '${state[`${stateName}_${variable[0]}`]}'** but has now been **set to its default** value, '${defaultValue}'.`, -1);
+                    state[`${stateName}_${variable[0]}`] = defaultValue;
+                }
+            })
+        },
+        
+        registerEventHandlers = function () {
+            on('chat:message', handleInput);
+        };
+
+    return {
+        CheckMacros:checkMacros,
+        StartupChecks:startupChecks,
+        RegisterEventHandlers:registerEventHandlers
+    };
+}())
+
+on('ready', function () {
+    APIName.CheckMacros();
+    APIName.StartupChecks();
+    APIName.RegisterEventHandlers();
+})
